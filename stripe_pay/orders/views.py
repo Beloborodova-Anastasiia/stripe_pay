c79a3ea043a0f919d4ebf33b72e7d1528c23ad2d
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2023-02-14 14:47:29 +0500

    Edit worflow. Push docker image to docker hub

--- stripe_pay/orders/views.py
+++ stripe_pay/orders/views.py
@@ -37,10 +37,10 @@ def item_detail(request, item_id):
 @api_view(['GET',])
 def create_checkout_session(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-    host = request.get_host()
+    # host = request.get_host()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    sok = s.getsockname()[0]
+    sock = s.getsockname()[0]
     s.close()
     session = stripe.checkout.Session.create(
         line_items=[{
@@ -54,8 +54,8 @@ def create_checkout_session(request, item_id):
             'quantity': QUANTITY,
         }],
         mode=MODE,
-        success_url='http://' + sok + reverse('orders:success'),
-        cancel_url='http://' + sok + reverse('orders:cancel'),
+        success_url='http://' + sock + reverse('orders:success'),
+        cancel_url='http://' + sock + reverse('orders:cancel'),
     )
     data = {
         'id': session.id
