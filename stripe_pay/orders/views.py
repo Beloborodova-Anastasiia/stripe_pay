d8b58d0ceb2983920c3dda02f97d6c2ad2005fc7
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2023-02-14 20:37:11 +0500

    edit view

--- stripe_pay/orders/views.py
+++ stripe_pay/orders/views.py
@@ -1,5 +1,5 @@
 import os
-# import socket
+import socket
 
 import stripe
 from django.shortcuts import get_object_or_404, render
@@ -36,10 +36,11 @@ def item_detail(request, item_id):
 def create_checkout_session(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     host = request.get_host()
-    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # s.connect(("8.8.8.8", 80))
-    # sock = s.getsockname()[0]
-    # s.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    sock = s.getsockname()[0]
+    
+    s.close()
     session = stripe.checkout.Session.create(
         line_items=[{
             'price_data': {
@@ -53,7 +54,7 @@ def create_checkout_session(request, item_id):
         }],
         mode=MODE,
         success_url='http://' + host + reverse('orders:success'),
-        cancel_url='http://' + host + reverse('orders:cancel'),
+        cancel_url='http://' + sock + reverse('orders:cancel'),
     )
     data = {
         'id': session.id
